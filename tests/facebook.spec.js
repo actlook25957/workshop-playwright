2fe96910f14d7dfe4c17ecefacdd48e222bfe31c
Traitawat Jitchana <60166895+actlook25957@users.noreply.github.com>
2024-03-20 15:00:59 +0700

    Update facebook.spec.js

--- tests/facebook.spec.js
+++ tests/facebook.spec.js
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test';
 test('Post Hello Playwright onfacebook', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
   await page.getByTestId('royal_email').click();
-  await page.getByTestId('royal_email').fill('actlookfororigin@gmail.ocn');
+  await page.getByTestId('royal_email').fill('xxxxxxxxxx');
   await page.getByTestId('royal_pass').click();
-  await page.getByTestId('royal_pass').fill('lookKmutt@25957');
+  await page.getByTestId('royal_pass').fill('xxxxxxxxxx');
   await page.getByTestId('royal_login_button').click();
   await page.getByRole('link', { name: 'Facebook' }).click();
   await page.getByRole('button', { name: 'What\'s on your mind,' }).click();
